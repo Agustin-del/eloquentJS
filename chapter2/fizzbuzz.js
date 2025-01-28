854129c9d2956a6b33875d09917942de06c8a79b
Agustin-del <eddyruveda@gmail.com>
2025-01-28 15:21:16 -0300

    volviendo

--- chapter2/fizzbuzz.js
+++ chapter2/fizzbuzz.js
@@ -1,15 +1,7 @@
 for(let i = 1; i <= 100; i++) {
-    
-    if( i % 5 === 0 && i % 3 === 0) {
-        console.log('FizzBuzz')
-        continue
-    } else if(i % 3 === 0) {
-        console.log('Fizz')
-        continue
-    } else if( i % 5 === 0) {
-        console.log('Buzz')
-        continue
-    } 
+    let output = ''    
+    if(i % 3 === 0) output += 'Fizz'
+    if(i % 5 === 0) output += 'Buzz'
 
-    console.log(i)
+    console.log(output || i)
 } 
