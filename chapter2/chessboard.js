44be9b86053d0a3782ef046ad1a403fe2a111af9
Agustin-del <eddyruveda@gmail.com>
2025-01-28 15:17:42 -0300

    volviendo

---
+++ chapter2/chessboard.js
@@ -0,0 +1,14 @@
+let chessboard = ''
+let size = 8
+for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+        if ((row + col) % 2 === 0) {
+            chessboard += ' '
+        } else {
+            chessboard += '#'
+        }
+    }
+    chessboard += '\n'
+}
+
+console.log(chessboard)
